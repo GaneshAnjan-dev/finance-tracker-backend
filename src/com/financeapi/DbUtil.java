b9d81934810dc18ff57e5c6b2d3df88d724ceda2
Ganesh Anjan G <anjanshetty65@gmail.com>
2025-12-07 17:03:29 +0530

    Update DbUtil.java

--- src/com/financeapi/DbUtil.java
+++ src/com/financeapi/DbUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class DbUtil {
 
-    // Change these if your DB user/password are different
+    // Change these if your DB user/password are different//
     private static final String URL = "jdbc:postgresql://localhost:5432/finance_db";
     private static final String USER = "postgres";
     private static final String PASSWORD = "12345";
